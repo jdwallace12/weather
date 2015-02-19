10d8dd6d3ad301f4ee43e6133d42542df866d238
John Wallace <jdwallace12@gmail.com>
2015-02-19 01:27:28 -0500

    got wind in showing up

--- client/layout.js
+++ client/layout.js
@@ -2,10 +2,20 @@ Template.layout.helpers({
     curTemp: function() {
         var city = Session.get('city');
         Meteor.call('getTemps', city, function(err, results) {
-            console.log(results.content);
+     
             Session.set('temps',JSON.parse(results.content).main.temp);
         });
-        return (Session.get('temps') + ' degrees ' + ' in ' + city );
+        return (Session.get('temps') + ' degrees ' + ' in ' + city);
+    }
+});
+
+Template.layout.helpers({
+    windSpeed: function() {
+        var city = Session.get('city');
+        Meteor.call('getWind', city, function(err, results) {
+            Session.set('winds',JSON.parse(results.content).wind.speed);
+        });
+        return (Session.get('winds') + ' mph');
     }
 });
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 Meteor.methods({
 	'getTemps':function(city){
 		return Meteor.http.call('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + city  + "&units=imperial");
-	}
+	},
+		'getWind':function(city){
+		return Meteor.http.call('GET', 'http://api.openweathermap.org/data/2.5/weather?q=' + city  + "&units=imperial");
+	},
+	
+
 });
